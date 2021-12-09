2b47fc0bfde7f6fae4ab8978d3c8bb74f4427657
jitarth <jitarth@codebtech.com>
2021-12-09 17:05:29 +0530

    - changes in app.dart

--- lib/di/usecase/payment/payment_usecase_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:domain/usecase/payment/add_send_money_contact_usecase.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-///[<AddSendMoneyContactUseCaseProvider>] provider
-final addSendMoneyContactUseCaseProvider =
-    Provider.autoDispose<AddSendMoneyContactUseCase>(
-  (ref) => AddSendMoneyContactUseCase(),
-);
--- lib/feature/payment/add_send_money_contact/add_send_money_contact_view_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:domain/usecase/payment/add_send_money_contact_usecase.dart';
-import 'package:neo_bank/base/base_page_view_model.dart';
-
-class AddSendMoneyContactViewModel extends BasePageViewModel {
-  AddSendMoneyContactUseCase _useCase;
-
-  AddSendMoneyContactViewModel(this._useCase);
-}
