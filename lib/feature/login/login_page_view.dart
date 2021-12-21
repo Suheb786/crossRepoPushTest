b47d03a6303e6826fa1732d270175d831715861d
Santoshi Bisht <santoshi@codebtech.com>
2021-12-21 11:11:52 +0530

    --login navigation

--- lib/feature/login/login_page_view.dart
+++ lib/feature/login/login_page_view.dart
@@ -82,11 +82,10 @@ class LoginPageView extends BasePageViewWidget<LoginViewModel> {
                                         .getToken();
                                     //model.checkKycStatus();
                                     model.emailKey.currentState!.isValid = true;
-                                    ///TODO: change routing to onboarding
                                     Future.delayed(Duration(milliseconds: 500),
                                         () {
                                       Navigator.pushReplacementNamed(
-                                          context, RoutePaths.OnBoarding);
+                                          context, RoutePaths.AppHome);
                                     });
                                   } else if (data.status == Status.ERROR) {
                                     model.emailKey.currentState!.isValid =
