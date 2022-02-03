a24d0b5403e3b1ff5ba1a3d3891318d9212ac516
Dhaval Parmar <dhaval@codebtech.com>
2022-02-03 18:02:11 +0530

    -- minor fixes

--- lib/feature/register/register_page.dart
+++ lib/feature/register/register_page.dart
@@ -5,6 +5,7 @@ import 'package:neo_bank/base/base_page.dart';
 import 'package:neo_bank/di/register/register_modules.dart';
 import 'package:neo_bank/feature/register/register_page_model.dart';
 import 'package:neo_bank/feature/register/register_page_view.dart';
+import 'package:neo_bank/main/navigation/route_paths.dart';
 
 class RegisterPage extends BasePage<RegisterViewModel> {
   final RegisterPageParams arguments;
@@ -22,126 +23,126 @@ class RegisterPageState
     return registerViewModelProvider;
   }
 
-  // @override
-  // void onModelReady(RegisterViewModel model) {
-  //   super.onModelReady(model);
-  //   if (widget.arguments.kycData != null) {
-  //     switch (widget.arguments?.kycData?.type ?? "") {
-  //       case "IDCardC":
-  //       case "SelfiCheck":
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           model.navigateToPage(0);
-  //         });
-  //
-  //         break;
-  //       case "CountryResidence":
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           model.navigateToPage(0);
-  //         });
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           ProviderScope.containerOf(context)
-  //               .read(registerStepOneViewModelProvider)
-  //               .pageController
-  //               .move(2, animation: false);
-  //         });
-  //         break;
-  //       case "ProfileStatus":
-  //       case "JobDetails":
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           model.navigateToPage(0);
-  //         });
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           ProviderScope.containerOf(context)
-  //               .read(registerStepOneViewModelProvider)
-  //               .pageController
-  //               .move(3, animation: false);
-  //         });
-  //         break;
-  //       case "AccountOpeningPurpose":
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           model.navigateToPage(2);
-  //         });
-  //
-  //         break;
-  //       case "FatcaCrs":
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           model.navigateToPage(3);
-  //         });
-  //
-  //         break;
-  //
-  //       case "W8":
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           model.navigateToPage(3);
-  //         });
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           ProviderScope.containerOf(context)
-  //               .read(registerStepFourViewModelProvider)
-  //               .registrationStepFourPageController
-  //               .move(1, animation: false);
-  //         });
-  //
-  //         break;
-  //
-  //       case "W9":
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           model.navigateToPage(3);
-  //         });
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           ProviderScope.containerOf(context)
-  //               .read(registerStepFourViewModelProvider)
-  //               .registrationStepFourPageController
-  //               .move(4, animation: false);
-  //         });
-  //
-  //         break;
-  //
-  //       case "UploadDocument":
-  //         // print('iamhere');
-  //         // Future.delayed(Duration(microseconds: 100), () {
-  //         //   model.navigateToPage(4);
-  //         // });
-  //         // print('i am here 1');
-  //         // Future.delayed(Duration(microseconds: 100), () {
-  //         //   print('i am here 2');
-  //         //   ProviderScope.containerOf(context)
-  //         //       .read(registerStepFiveViewModelProvider)
-  //         //       .registrationStepFivePageController
-  //         //       .move(4, animation: false);
-  //         // });
-  //         // Navigator.pushReplacementNamed(
-  //         //     context, RoutePaths.UploadDocumentLater);
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           Navigator.pushReplacementNamed(
-  //               context, RoutePaths.UploadDocumentLater);
-  //         });
-  //
-  //         break;
-  //
-  //       case "AccountInfo":
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           model.navigateToPage(4);
-  //         });
-  //
-  //         break;
-  //
-  //       case "debitCardIssuance":
-  //       case "SetPINdebitCard":
-  //         Future.delayed(Duration(microseconds: 100), () {
-  //           Navigator.pushReplacementNamed(context, RoutePaths.CardDelivery);
-  //         });
-  //
-  //         break;
-  //
-  //       default:
-  //         Navigator.pushReplacementNamed(context, RoutePaths.AppHome);
-  //         break;
-  //     }
-  //   }
-  //   // Future.delayed(Duration(microseconds: 100), () {
-  //   //   model.navigateToPage(3);
-  //   // });
-  // }
+  @override
+  void onModelReady(RegisterViewModel model) {
+    super.onModelReady(model);
+    if (widget.arguments.kycData != null) {
+      switch (widget.arguments.kycData?.type ?? "") {
+        case "IDCardC":
+        case "SelfiCheck":
+          Future.delayed(Duration(microseconds: 100), () {
+            model.navigateToPage(0);
+          });
+
+          break;
+        case "CountryResidence":
+          Future.delayed(Duration(microseconds: 100), () {
+            model.navigateToPage(0);
+          });
+          Future.delayed(Duration(microseconds: 100), () {
+            ProviderScope.containerOf(context)
+                .read(registerStepOneViewModelProvider)
+                .pageController
+                .move(2, animation: false);
+          });
+          break;
+        case "ProfileStatus":
+        case "JobDetails":
+          Future.delayed(Duration(microseconds: 100), () {
+            model.navigateToPage(0);
+          });
+          Future.delayed(Duration(microseconds: 100), () {
+            ProviderScope.containerOf(context)
+                .read(registerStepOneViewModelProvider)
+                .pageController
+                .move(3, animation: false);
+          });
+          break;
+        case "AccountOpeningPurpose":
+          Future.delayed(Duration(microseconds: 100), () {
+            model.navigateToPage(2);
+          });
+
+          break;
+        case "FatcaCrs":
+          Future.delayed(Duration(microseconds: 100), () {
+            model.navigateToPage(3);
+          });
+
+          break;
+
+        case "W8":
+          Future.delayed(Duration(microseconds: 100), () {
+            model.navigateToPage(3);
+          });
+          Future.delayed(Duration(microseconds: 100), () {
+            ProviderScope.containerOf(context)
+                .read(registerStepFourViewModelProvider)
+                .registrationStepFourPageController
+                .move(1, animation: false);
+          });
+
+          break;
+
+        case "W9":
+          Future.delayed(Duration(microseconds: 100), () {
+            model.navigateToPage(3);
+          });
+          Future.delayed(Duration(microseconds: 100), () {
+            ProviderScope.containerOf(context)
+                .read(registerStepFourViewModelProvider)
+                .registrationStepFourPageController
+                .move(4, animation: false);
+          });
+
+          break;
+
+        case "UploadDocument":
+          // print('iamhere');
+          // Future.delayed(Duration(microseconds: 100), () {
+          //   model.navigateToPage(4);
+          // });
+          // print('i am here 1');
+          // Future.delayed(Duration(microseconds: 100), () {
+          //   print('i am here 2');
+          //   ProviderScope.containerOf(context)
+          //       .read(registerStepFiveViewModelProvider)
+          //       .registrationStepFivePageController
+          //       .move(4, animation: false);
+          // });
+          // Navigator.pushReplacementNamed(
+          //     context, RoutePaths.UploadDocumentLater);
+          Future.delayed(Duration(microseconds: 100), () {
+            Navigator.pushReplacementNamed(
+                context, RoutePaths.UploadDocumentLater);
+          });
+
+          break;
+
+        case "AccountInfo":
+          Future.delayed(Duration(microseconds: 100), () {
+            model.navigateToPage(4);
+          });
+
+          break;
+
+        case "debitCardIssuance":
+        case "SetPINdebitCard":
+          Future.delayed(Duration(microseconds: 100), () {
+            Navigator.pushReplacementNamed(context, RoutePaths.CardDelivery);
+          });
+
+          break;
+
+        default:
+          Navigator.pushReplacementNamed(context, RoutePaths.AppHome);
+          break;
+      }
+    }
+    // Future.delayed(Duration(microseconds: 100), () {
+    //   model.navigateToPage(3);
+    // });
+  }
 
   @override
   Widget buildView(BuildContext context, RegisterViewModel model) {
