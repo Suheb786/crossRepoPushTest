f69b851e5b08c9b331f7203394025e79db8552b7
Santoshi Bisht <santoshi@codebtech.com>
2022-03-11 13:09:06 +0530

    --crash analytics issue fixes WIP

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:data/network/utils/app_http_overrides.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 // import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,22 +12,22 @@ import 'package:neo_bank/main/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runZoned(() {
-    HttpOverrides.global = AppHttpOverrides();
-    runApp(ProviderScope(child: MyApp()));
-  });
-  // await runZonedGuarded(() async {
+  // runZoned(() {
   //   HttpOverrides.global = AppHttpOverrides();
-  //   await Firebase.initializeApp();
-  //
-  //   // Pass all uncaught errors from the framework to Crashlytics.
-  //   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-  //   await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-  //
   //   runApp(ProviderScope(child: MyApp()));
-  // }, (error, stackTrace) async {
-  //   // FirebaseCrashlytics.instance.recordError(error, stackTrace);
   // });
+  await runZonedGuarded(() async {
+    HttpOverrides.global = AppHttpOverrides();
+    await Firebase.initializeApp();
+
+    // Pass all uncaught errors from the framework to Crashlytics.
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+
+    runApp(ProviderScope(child: MyApp()));
+  }, (error, stackTrace) async {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
 }
 
 class MyApp extends StatefulWidget {
