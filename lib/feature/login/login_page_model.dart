fb2d827342c30eb6afd405cc96194db6fac7e9bd
Santoshi Bisht <santoshi@codebtech.com>
2022-06-28 17:00:51 +0530

    --minor fixes

--- lib/feature/login/login_page_model.dart
+++ lib/feature/login/login_page_model.dart
@@ -43,8 +43,8 @@ class LoginViewModel extends BasePageViewModel {
   final GenerateKeyPairUseCase _generateKeyPairUseCase;
   final InfobipMessagePluginUseCase _infobipMessagePluginUseCase;
 
-  final TextEditingController emailController = TextEditingController(text:'testrob122@g.com');
-  final TextEditingController passwordController = TextEditingController(text:'Asdf123!');
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
   final ScrollController scrollController = ScrollController();
   final GlobalKey<AppTextFieldState> emailKey = GlobalKey(debugLabel: "login_email");
   final GlobalKey<AppTextFieldState> passwordKey = GlobalKey(debugLabel: "login_password");
