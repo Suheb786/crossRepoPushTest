e8b0864d5bbe7548cdb34ff343df4b501ef12385
Vivek <vivek@codebtech.com>
2023-04-01 22:27:58 +0530

    firebase log rtp transaction,error mapping adding rigght arrow in payment activity list item

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -47,8 +47,8 @@ class MessageLookup extends MessageLookupByLibrary {
   static String m12(noOfTransaction) => "نوع الحركة ${noOfTransaction}";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) =>
-      <String, Function>{
+
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "AccountIsLinkedToTheAlias": MessageLookupByLibrary.simpleMessage(
             "الحساب مربوط الى الاسم المستعار، لكن لم ننجح من جعله الحساب الرئيسي"),
         "CustomerOfInvalidAlias": MessageLookupByLibrary.simpleMessage("الاسم المستعار غير صحيح"),
