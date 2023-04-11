acf91b9a6868a8e7ac49d86a7adabfdd5ddcb91d
Vivek <vivek@codebtech.com>
2023-04-11 18:40:18 +0530

    integration share credit confirmation api

---
+++ data/lib/entity/remote/cliq/credit_confirmation/credit_confirmation_entity.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'credit_confirmation_entity.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CreditConfirmationEntity _$CreditConfirmationEntityFromJson(Map<String, dynamic> json) {
+  return CreditConfirmationEntity(
+    json['pdf'] ?? '',
+  );
+}
+
+Map<String, dynamic> _$CreditConfirmationEntityToJson(CreditConfirmationEntity instance) => <String, dynamic>{
+      'pdf': instance.pdf,
+    };
