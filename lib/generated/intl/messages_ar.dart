15432bf6967f6515bba637bda036a61f357421da
Vivek <vivek@codebtech.com>
2023-04-13 19:23:14 +0530

    replacing + with 00 in dummyMobileNo key in arb file

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -49,8 +49,7 @@ class MessageLookup extends MessageLookupByLibrary {
   static String m13(noOfTransaction) => "نوع الحركة ${noOfTransaction}";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) =>
-      <String, Function>{
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "AccountIsLinkedToTheAlias": MessageLookupByLibrary.simpleMessage(
             "الحساب مربوط الى الاسم المستعار، لكن لم ننجح من جعله الحساب الرئيسي"),
         "CustomerOfInvalidAlias": MessageLookupByLibrary.simpleMessage("الاسم المستعار غير صحيح"),
