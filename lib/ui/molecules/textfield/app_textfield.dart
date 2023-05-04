4b29cca3f1df09740c46690346153d1820bbb90e
Bhavy Koshti <bhavy.koshti@codebtech.com>
2023-05-04 19:34:43 +0530

    Update -- search field firebase analytics error and circular avtar changes.

--- lib/ui/molecules/textfield/app_textfield.dart
+++ lib/ui/molecules/textfield/app_textfield.dart
@@ -170,19 +170,21 @@ class AppTextFieldState extends State<AppTextField> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      RichText(
-                        text: TextSpan(children: [
-                          TextSpan(
-                              text: widget.labelText,
-                              style: TextStyle(
-                                color: widget.labelColor ??
-                                    Theme.of(context).inputDecorationTheme.labelStyle!.color,
-                                fontSize: 10.0.t,
-                                fontFamily: StringUtils.appFont,
-                              )),
-                          WidgetSpan(child: widget.labelIcon?.call() ?? SizedBox.shrink())
-                        ]),
-                      ),
+                      widget.labelText.isNotEmpty
+                          ? RichText(
+                              text: TextSpan(children: [
+                                TextSpan(
+                                    text: widget.labelText,
+                                    style: TextStyle(
+                                      color: widget.labelColor ??
+                                          Theme.of(context).inputDecorationTheme.labelStyle!.color,
+                                      fontSize: 10.0.t,
+                                      fontFamily: StringUtils.appFont,
+                                    )),
+                                WidgetSpan(child: widget.labelIcon?.call() ?? SizedBox.shrink())
+                              ]),
+                            )
+                          : SizedBox.shrink(),
                       TextFormField(
                         focusNode: _focusNode,
                         maxLength: widget.maxLength,
