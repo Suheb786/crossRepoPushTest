b35b7f96bcb878ed49a14957a470b62e5575bc0b
Santoshi Bisht <santoshi@codebtech.com>
2023-07-05 22:03:43 +0530

    --feature flag handling

--- data/lib/entity/remote/contact/beneficiary_transaction_history_content_entity.g.dart
+++ data/lib/entity/remote/contact/beneficiary_transaction_history_content_entity.g.dart
@@ -6,15 +6,17 @@ part of 'beneficiary_transaction_history_content_entity.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-BeneficiaryTransactionHistoryContentEntity _$BeneficiaryTransactionHistoryContentEntityFromJson(
-        Map<String, dynamic> json) =>
-    BeneficiaryTransactionHistoryContentEntity(
-      date: json['date'] as String? ?? '',
-      data: (json['data'] as List<dynamic>?)
-              ?.map((e) => BeneficiaryTransactionHistoryEntity.fromJson(e as Map<String, dynamic>))
-              .toList() ??
-          [],
-    );
+BeneficiaryTransactionHistoryContentEntity
+    _$BeneficiaryTransactionHistoryContentEntityFromJson(
+            Map<String, dynamic> json) =>
+        BeneficiaryTransactionHistoryContentEntity(
+          date: json['date'] as String? ?? '',
+          data: (json['data'] as List<dynamic>?)
+                  ?.map((e) => BeneficiaryTransactionHistoryEntity.fromJson(
+                      e as Map<String, dynamic>))
+                  .toList() ??
+              [],
+        );
 
 Map<String, dynamic> _$BeneficiaryTransactionHistoryContentEntityToJson(
         BeneficiaryTransactionHistoryContentEntity instance) =>
--- data/lib/entity/remote/contact/beneficiary_transaction_history_response_entity.g.dart
+++ data/lib/entity/remote/contact/beneficiary_transaction_history_response_entity.g.dart
@@ -6,12 +6,15 @@ part of 'beneficiary_transaction_history_response_entity.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-BeneficiaryTransactionHistoryResponseEntity _$BeneficiaryTransactionHistoryResponseEntityFromJson(
-        Map<String, dynamic> json) =>
-    BeneficiaryTransactionHistoryResponseEntity(
-      response:
-          json['response'] == null ? null : ResponseEntity.fromJson(json['response'] as Map<String, dynamic>),
-    );
+BeneficiaryTransactionHistoryResponseEntity
+    _$BeneficiaryTransactionHistoryResponseEntityFromJson(
+            Map<String, dynamic> json) =>
+        BeneficiaryTransactionHistoryResponseEntity(
+          response: json['response'] == null
+              ? null
+              : ResponseEntity.fromJson(
+                  json['response'] as Map<String, dynamic>),
+        );
 
 Map<String, dynamic> _$BeneficiaryTransactionHistoryResponseEntityToJson(
         BeneficiaryTransactionHistoryResponseEntity instance) =>
