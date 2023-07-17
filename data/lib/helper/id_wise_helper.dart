08cba243cd9f5f0837f07cad9c227a0ae9d2e8d8
Bhavy Koshti <bhavy.koshti@codebtech.com>
2023-07-17 14:00:24 +0530

    Update -- changes in demo.

--- data/lib/helper/id_wise_helper.dart
+++ data/lib/helper/id_wise_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -7,6 +9,7 @@ enum IDWiseStatus {
   ERROR,
   STARTED,
   RESUMED,
+  ALREADY_STARTED,
 }
 
 class IdWiseHelper {
@@ -24,53 +27,62 @@ class IdWiseHelper {
   final clientId =
       "QmFzaWMgWkdKbU5qZ3dNMkl0Tm1ZNVlTMDBOVEJrTFRrMk16RXRPVGs0WWpZMFptVTROekZpT2t4Sk4xTjNPR3hoUm5wNGFqUk5NbWRZVVVSek1rNWFhRlJDU0dObU9XTlhTakIwZGpaWlVWVT0=";
 
-  final journeyDefinitionId = "dbf6803b-6f9a-450d-9631-998b64fe871b";
+  final journeyDefinitionId = "dbf6803b-6f9a-450d-9631-998b64fe871";
 
   void initializeIdWise() {
     platformChannel.invokeMethod('initialize', {"clientKey": clientId, "theme": "LIGHT"});
   }
 
-  Future<IDWiseStatus> startVerification(String lang) async {
-    var journeyResult = await platformChannel.invokeMethod('startJourney', {
-      "locale": lang,
-      "referenceNo": "",
-      "journeyDefinitionId": journeyDefinitionId,
-    });
-
+  Future<Map<IDWiseStatus, String>> startVerification(String lang) async {
+    Completer<Map<IDWiseStatus, String>> completer = Completer();
     platformChannel.setMethodCallHandler((handler) async {
       switch (handler.method) {
         case 'onJourneyStarted':
           print("Method: onJourneyStarted, ${handler.arguments.toString()}");
           break;
         case 'onJourneyFinished':
-          print("Method: onJourneyFinished");
+          print("Method: onJourneyFinished,${handler.arguments.toString()}");
+          completer.complete({IDWiseStatus.COMPLETED: handler.arguments.toString()});
           break;
+
         case 'onJourneyCancelled':
           print("Method: onJourneyCancelled,${handler.arguments.toString()}");
+          completer.complete({IDWiseStatus.CANCELLED: handler.arguments.toString()});
           break;
+
         case 'onJourneyResumed':
           print("Method: onJourneyResumed, ${handler.arguments.toString()}");
           break;
+
         case 'onError':
           print("Method: onError, ${handler.arguments.toString()}");
+          completer.complete({IDWiseStatus.ERROR: handler.arguments.toString()});
           break;
+
         default:
           print('Unknown method from MethodChannel: ${handler.method}');
+          completer.complete({IDWiseStatus.ERROR: handler.arguments.toString()});
           break;
       }
     });
 
-    int statusCode = int.parse(journeyResult['statusCode'].toString());
-    debugPrint("CODE ----- ${statusCode.getIDWiseStatusFromCode().toString()}");
-    debugPrint("DATA ----- ${journeyResult['status']}");
+    platformChannel.invokeMethod('startJourney', {
+      "locale": lang,
+      "referenceNo": "",
+      "journeyDefinitionId": journeyDefinitionId,
+    });
 
-    return statusCode.getIDWiseStatusFromCode();
+    // int statusCode = int.parse(journeyResult['statusCode'].toString());
+    // debugPrint("CODE ----- ${statusCode.getIDWiseStatusFromCode().toString()}");
+    // debugPrint("DATA ----- ${journeyResult['status']}");
+
+    return completer.future;
   }
 
 //  64476a7d6cc36a530ab19976
 }
 
-extension on int {
+/*extension on int {
   IDWiseStatus getIDWiseStatusFromCode() {
     switch (this) {
       case 0:
@@ -87,4 +99,4 @@ extension on int {
         return IDWiseStatus.ERROR;
     }
   }
-}
+}*/
