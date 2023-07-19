b05a4c1afb2ed6b994b1fd32a525a41ac9b0c727
Bhavy Koshti <bhavy.koshti@codebtech.com>
2023-07-19 15:30:48 +0530

    Update -- idwise changes done.

--- scripts/ios_release.sh
+++ scripts/ios_release.sh
@@ -1,7 +1,8 @@
 cd /Users/neosoft/StudioProjects/neobank/lib || exit
-fvm flutter clean
-fvm flutter pub get
+#fvm flutter clean
+#fvm flutter pub get
 cd /Users/neosoft/StudioProjects/neobank/ios || exit
-pod repo update
-pod install
+#pod repo update
+#pod install
+fvm flutter run -release
 #flutter build appbundle --release --no-tree-shake-icons
