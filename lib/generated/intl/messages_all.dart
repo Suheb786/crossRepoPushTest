61c98c2e558a5c1adab40cc165cd579eab85d010
Bhavy Koshti <bhavy.koshti@codebtech.com>
2023-08-24 11:44:17 +0530

    Update - merge conflict resolving.

--- lib/generated/intl/messages_all.dart
+++ lib/generated/intl/messages_all.dart
@@ -37,7 +37,7 @@ MessageLookupByLibrary? _findExact(String localeName) {
 }
 
 /// User programs should call this before using [localeName] for messages.
-Future<bool> initializeMessages(String localeName) async {
+Future<bool> initializeMessages(String localeName) {
   var availableLocale = Intl.verifiedLocale(
       localeName, (locale) => _deferredLibraries[locale] != null,
       onFailure: (_) => null);
