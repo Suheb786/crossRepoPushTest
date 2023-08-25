cdea097b1bb96ab42260ba5dfc452708bf52262b
Mohammad Suheb <suheb.mohammad@codebtech.com>
2023-08-25 19:47:08 +0530

    open sub account handling commented out

--- lib/feature/dashboard_home/app_home/app_home_page_view_new.dart
+++ lib/feature/dashboard_home/app_home/app_home_page_view_new.dart
@@ -311,23 +311,21 @@ class AppHomePageViewNew extends BasePageViewWidget<AppHomeViewModel> {
                                                                                   model.isPrimaryDebitCard,
                                                                               debitCard:
                                                                                   model.selectedDebitCard!,
-                                                                              debitCardRequestPhysicalCardEnabled:
-                                                                                  cardData
-                                                                                          ?.data
-                                                                                          ?.dashboardDataContent
-                                                                                          ?.dashboardFeatures
-                                                                                          ?.isDebitCardRequestPhysicalCardEnabled ??
-                                                                                      false))
-                                                                          : CreditCardSettingsPage(
-                                                                              CreditCardSettingsArguments(
-                                                                                  creditCard: model
-                                                                                      .selectedCreditCard!,
-                                                                                  isChangePinEnabled: cardData
-                                                                                          ?.data
-                                                                                          ?.dashboardDataContent
-                                                                                          ?.dashboardFeatures
-                                                                                          ?.isPinChangeEnabled ??
-                                                                                      true))
+                                                                              debitCardRequestPhysicalCardEnabled: cardData
+                                                                                      ?.data
+                                                                                      ?.dashboardDataContent
+                                                                                      ?.dashboardFeatures
+                                                                                      ?.isDebitCardRequestPhysicalCardEnabled ??
+                                                                                  false))
+                                                                          : CreditCardSettingsPage(CreditCardSettingsArguments(
+                                                                              creditCard:
+                                                                                  model.selectedCreditCard!,
+                                                                              isChangePinEnabled: cardData
+                                                                                      ?.data
+                                                                                      ?.dashboardDataContent
+                                                                                      ?.dashboardFeatures
+                                                                                      ?.isPinChangeEnabled ??
+                                                                                  true))
                                                                       : switchedPage == DashboardAnimatedPage.TIMELINE
                                                                           ? DebitCardTimeLinePage(TimeLinePageArguments(cardType: model.cardTypeList[currentStep ?? 0].cardType, timeLineArguments: model.timeLineArguments))
                                                                           : switchedPage == DashboardAnimatedPage.PAYBACK
@@ -418,33 +416,28 @@ class AppHomePageViewNew extends BasePageViewWidget<AppHomeViewModel> {
                                                                                                     ),
                                                                                               ),
                                                                                               OpenSubAccountTile(
-                                                                                                isCardActivated:
-                                                                                                    true,
-                                                                                                isEnabled:
-                                                                                                    true,
-
-                                                                                                // isCardActivated: (cardData
-                                                                                                //             ?.data
-                                                                                                //             ?.dashboardDataContent
-                                                                                                //             ?.dashboardFeatures
-                                                                                                //             ?.subAccountFeature ??
-                                                                                                //         false) &&
-                                                                                                //     (cardData
-                                                                                                //             ?.data
-                                                                                                //             ?.dashboardDataContent
-                                                                                                //             ?.allowSubAccount ??
-                                                                                                //         false),
-                                                                                                // isEnabled: (cardData
-                                                                                                //             ?.data
-                                                                                                //             ?.dashboardDataContent
-                                                                                                //             ?.dashboardFeatures
-                                                                                                //             ?.subAccountFeature ??
-                                                                                                //         false) &&
-                                                                                                //     (cardData
-                                                                                                //             ?.data
-                                                                                                //             ?.dashboardDataContent
-                                                                                                //             ?.allowSubAccount ??
-                                                                                                //         false),
+                                                                                                isCardActivated: (cardData
+                                                                                                            ?.data
+                                                                                                            ?.dashboardDataContent
+                                                                                                            ?.dashboardFeatures
+                                                                                                            ?.subAccountFeature ??
+                                                                                                        false) &&
+                                                                                                    (cardData
+                                                                                                            ?.data
+                                                                                                            ?.dashboardDataContent
+                                                                                                            ?.allowSubAccount ??
+                                                                                                        false),
+                                                                                                isEnabled: (cardData
+                                                                                                            ?.data
+                                                                                                            ?.dashboardDataContent
+                                                                                                            ?.dashboardFeatures
+                                                                                                            ?.subAccountFeature ??
+                                                                                                        false) &&
+                                                                                                    (cardData
+                                                                                                            ?.data
+                                                                                                            ?.dashboardDataContent
+                                                                                                            ?.allowSubAccount ??
+                                                                                                        false),
                                                                                                 model: model,
                                                                                               ),
                                                                                               ShareAccountTile(
