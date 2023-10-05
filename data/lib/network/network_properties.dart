6534178acaa634da75725bed52941855fe4c23bb
Santoshi Bisht <santoshi@codebtech.com>
2023-10-05 21:56:20 +0530

    --V 2310051

--- data/lib/network/network_properties.dart
+++ data/lib/network/network_properties.dart
@@ -19,6 +19,7 @@ class NetworkProperties {
 // static const String BASE_BENEFICIARY_URL = "https://mobwebuat1.theblink.com:3443/contactcenter/api";
 // static const String BASE_EV0UCHER_URL = "https://mobwebuat1.theblink.com:3443/Voucher/api";
 // static const String BASE_OFFER_CAMPAIGN_URL = "https://mobwebuat1.theblink.com:3443/Offers/api";
+//static const String BASE_RJ_URL = "https://mobwebuat1.theblink.com:3443/RJAPI/api";
 
   ///VAPT / PREPROD CONFIGURATION (UAT 2 )
 // static const String BASE_CHANNEL_URL = "https://mobwebuat2.theblink.com:4443/api";
@@ -26,6 +27,7 @@ class NetworkProperties {
 // static const String BASE_BENEFICIARY_URL = "https://mobwebuat2.theblink.com:4443/contactcenter/api";
 // static const String BASE_EV0UCHER_URL = "https://mobwebuat2.theblink.com:4443/Voucher/api";
 // static const String BASE_OFFER_CAMPAIGN_URL = "https://mobwebuat2.theblink.com:4443/Offers/api";
+//static const String BASE_RJ_URL = "https://mobwebuat2.theblink.com:4443/RJAPI/api";
 
   ///PROD CONFIGURATION
 // static const String BASE_ROUTER_URL = "https://rout.theblink.com:5443/api";
@@ -33,4 +35,5 @@ class NetworkProperties {
 // static const String BASE_BENEFICIARY_URL = "https://mobweb.theblink.com:5443/contactcenter/api";
 // static const String BASE_EV0UCHER_URL = "https://mobweb.theblink.com:5443/Voucher/api";
 // static const String BASE_OFFER_CAMPAIGN_URL = "https://mobweb.theblink.com:5443/Offers/api";
+// static const String BASE_RJ_URL = "https://mobweb.theblink.com:5443/Offers/api";
 }
--- infobip_rtc_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ infobip_rtc_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import infobipplugin
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  InfobippluginPlugin.register(with: registry.registrar(forPlugin: "InfobippluginPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
