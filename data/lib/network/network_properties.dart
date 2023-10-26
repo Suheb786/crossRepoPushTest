9f3f38961d263b6ce1b75a861b84314deef880ec
Bhavy Koshti <Bhavy.Koshti@codebtech.com>
2023-10-26 17:46:01 +0530

    Update - Merge.

--- data/lib/network/network_properties.dart
+++ data/lib/network/network_properties.dart
@@ -5,6 +5,7 @@ class NetworkProperties {
   // static const String BASE_ROUTER_URL =
   //     "https://Rout-uat.theblink.com:2821/api";
 
+
   /// DEV CONFIGURATION FOR CHANNEL & ROUTER API
   static const String BASE_CHANNEL_URL = "http://10.6.13.2:2186/api";
   static const String BASE_ROUTER_URL = "https://10.6.13.1:2186/api";
