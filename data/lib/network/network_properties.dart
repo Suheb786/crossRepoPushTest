1f994aae73663465445ebc0d3fc1b0a3c11574cc
Bhavy Koshti <Bhavy.Koshti@codebtech.com>
2023-11-07 13:21:48 +0530

    Update - proper key setting for session timeout.

--- data/lib/network/network_properties.dart
+++ data/lib/network/network_properties.dart
@@ -1,8 +1,4 @@
 class NetworkProperties {
-  /// TIME OUT...
-  static Duration MAIN_TIMEOUT = Duration(minutes: 3);
-  static Duration WARNING_TIMEOUT = Duration(minutes: 1);
-
   /// UAT(RELEASE) CONFIGURATION FOR CHANNEL & ROUTER API
   // static const String BASE_CHANNEL_URL =
   //     "https://MobWeb-uat.theblink.com:2821/api";
@@ -27,13 +23,13 @@ class NetworkProperties {
 // static const String BASE_OFFER_CAMPAIGN_URL = "http://10.6.13.2:8190/Offers/api";
 
   ///VAPT / PREPROD CONFIGURATION (UAT 1 )
-  // static const String BASE_CHANNEL_URL = "https://mobwebuat1.theblink.com:3443/api";
-  // static const String BASE_ROUTER_URL = "https://routuat1.theblink.com:1443/api";
-  // static const String BASE_BENEFICIARY_URL = "https://mobwebuat1.theblink.com:3443/contactcenter/api";
-  // static const String BASE_EV0UCHER_URL = "https://mobwebuat1.theblink.com:3443/Voucher/api";
-  // static const String BASE_OFFER_CAMPAIGN_URL = "https://mobwebuat1.theblink.com:3443/Offers/api";
-  // static const String BASE_RJ_URL = "https://mobwebuat1.theblink.com:3443/RJAPI/api";
-  // static const String BASE_ONBOARDING_URL = "https://mobwebuat1.theblink.com:3443/Onboarding/api/Onboarding";
+// static const String BASE_CHANNEL_URL = "https://mobwebuat1.theblink.com:3443/api";
+// static const String BASE_ROUTER_URL = "https://routuat1.theblink.com:1443/api";
+// static const String BASE_BENEFICIARY_URL = "https://mobwebuat1.theblink.com:3443/contactcenter/api";
+// static const String BASE_EV0UCHER_URL = "https://mobwebuat1.theblink.com:3443/Voucher/api";
+// static const String BASE_OFFER_CAMPAIGN_URL = "https://mobwebuat1.theblink.com:3443/Offers/api";
+// static const String BASE_RJ_URL = "https://mobwebuat1.theblink.com:3443/RJAPI/api";
+// static const String BASE_ONBOARDING_URL = "https://mobwebuat1.theblink.com:3443/Onboarding/api/Onboarding";
 
   ///VAPT / PREPROD CONFIGURATION (UAT 2 )
 // static const String BASE_CHANNEL_URL = "https://mobwebuat2.theblink.com:4443/api";
